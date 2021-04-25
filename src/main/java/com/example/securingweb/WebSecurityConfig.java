c9b97b2175445948ad5fc7dda06a90d467f5a16f
deidra-JP <deidra@outlook.jp>
2021-04-25 20:05:53 +0900

    first commit

---
+++ src/main/java/com/example/securingweb/WebSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.example.securingweb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+      .authorizeRequests()
+        .antMatchers("/", "/home").permitAll()
+        .anyRequest().authenticated()
+        .and()
+      .formLogin()
+        .loginPage("/login")
+        .permitAll()
+        .and()
+      .logout()
+        .permitAll();
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    String password = passwordEncoder().encode("password");
+
+    auth.inMemoryAuthentication()
+        .passwordEncoder(passwordEncoder())
+        .withUser("user").password(password).roles("USER");
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+      return new BCryptPasswordEncoder();
+  }
+
+}
